dd7f1184266c408ba0cce9b79c2037da6632370f
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-09 16:09:50 +0200

    First draft of the Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Container, Button, Col } from "react-bootstrap";
 
-export default function Login() {
-  return <div>Login</div>;
+export default function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <div className="loginImage">
+      <Container className="loginGroup">
+        <Form as={Col} md={{ span: 6, offset: 3 }} className="loginForm">
+          <h1>Login</h1>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mt-5">
+            <Button variant="primary" type="submit" onClick={submitForm}>
+              Log in
+            </Button>
+          </Form.Group>
+          <Link
+            className="loginText"
+            to="/signup"
+            style={{ textAlign: "center" }}
+          >
+            New here? Sign up here
+          </Link>
+        </Form>
+      </Container>
+    </div>
+  );
 }
