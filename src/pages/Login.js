c7af9446f6623477c0659201c8ea3345450cf8af
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-10 10:26:47 +0200

    Created slice user in store (updated login page)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { login } from "../store/user/actions";
+import { selectToken } from "../store/user/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+
 import "./login.css";
 
 export default function LogIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token !== null) {
+      history.push("/");
+    }
+  }, [token, history]);
 
   function submitForm(event) {
+    console.log("hi");
     event.preventDefault();
+
+    dispatch(login(email, password));
 
     setEmail("");
     setPassword("");
---
+++ src/store/user/actions.js
@@ -0,0 +1,46 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+import {
+  appLoading,
+  appDoneLoading,
+  showMessageWithTimeout,
+  setMessage,
+} from "../appState/actions";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const TOKEN_STILL_VALID = "TOKEN_STILL_VALID";
+export const LOG_OUT = "LOG_OUT";
+
+const loginSuccess = (userWithToken) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: userWithToken,
+  };
+};
+
+export const logOut = () => ({ type: LOG_OUT });
+
+export const login = (email, password) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await axios.post(`${apiUrl}/login`, {
+        email,
+        password,
+      });
+
+      dispatch(loginSuccess(response.data));
+      dispatch(showMessageWithTimeout("success", false, "welcome back!", 1500));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        // console.log(error.response.data.message);
+        dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        // console.log(error.message);
+        dispatch(setMessage("danger", true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
