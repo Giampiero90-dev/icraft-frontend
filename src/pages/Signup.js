fe0ecd8e36063565f9ea7efed416f7c1d9cc1e78
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-10 13:08:47 +0200

    Signup page pure React

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
+
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [fullName, setFullName] = useState("");
+  const [bio, setBio] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    setFullName("");
+    setBio("");
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Signup</h1>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={fullName}
+            onChange={(event) => setFullName(event.target.value)}
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBio">
+          <Form.Label>Bio</Form.Label>
+          <Form.Control
+            value={bio}
+            onChange={(event) => setBio(event.target.value)}
+            type="text"
+            placeholder="Enter a short bio"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mt-5">
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Sign up
+          </Button>
+        </Form.Group>
+        <Link to="/login">Click here to log in</Link>
+      </Form>
+    </Container>
+  );
 }
