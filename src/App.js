5b9a5d0193bce4ceb6afa63152cb3014dc013826
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-10 18:13:49 +0200

    Navigation bar implemented  & functioning

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import logo from "./logo.svg";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
+import MessageBox from "./components/MessageBox";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
+import { useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+
 function App() {
+  const isLoading = useSelector(selectAppLoading);
+
   return (
     <div className="App">
+      <Navigation />
+      <MessageBox />
       <Switch>
+        {isLoading ? <Loading /> : null}
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        {/* <Route path="/main" component={MainPage} /> */}
-        {/* <Route path="/chat" component={DashBoard} /> */}
       </Switch>
     </div>
   );
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectToken } from "../../store/user/selectors";
+import NavbarItem from "./NavbarItem";
+import LoggedIn from "./LoggedIn";
+import LoggedOut from "./LoggedOut";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Navigation() {
+  const token = useSelector(selectToken);
+
+  const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={NavLink} to="/">
+        icraft
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav style={{ width: "100%" }} fill>
+          <NavbarItem path="/" linkText="Home" />
+          {/* {token ? (
+            <NavbarItem path="/addcreation" linkText="Add your creation" />
+          ) : null} */}
+          {loginLogoutControls}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
