33ca057e514c317da79cc98697cba7316f886a76
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-28 17:37:58 +0200

    Changed redux settings

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,13 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
+const enhancer = compose(
+  applyMiddleware(ReduxThunk),
+  window.navigator.userAgent.includes("Chrome")
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    : compose
+);
 
 const store = createStore(reducer, enhancer);
 
